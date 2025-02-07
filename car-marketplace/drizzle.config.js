1fbbf3b48b0739d23e70cbaa16356dd6ff7785a3
Javed Kathat <javedkhankathat3040@gmail.com>
2025-02-07 08:17:30 +0530

    Add New Listing data store to pg-db. now working on add images in this form.

---
+++ car-marketplace/drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type {import ("drizzle-kit").Config}*/
+export default {
+  schema: "./configs/schema.js", 
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://Javed%20Kathat:npg_SLkazgon69CH@ep-white-dream-a5y0o19x-pooler.us-east-2.aws.neon.tech/car-marketplace?sslmode=require",
+  },
+};
--- car-marketplace/src/add-listing/index.jsx
+++ car-marketplace/src/add-listing/index.jsx
@@ -9,10 +9,20 @@ import features from "@/Shared/features.json";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { db } from "../../configs";
+import { CarListing } from "../../configs/schema";
+import IconField from "./components/IconField";
 
 function AddListing() {
   const [formData, setFormData] = useState([]);
+  const [featuresData, setFeaturesData] = useState([]);
 
+
+  /**
+   * Save Input field Data using handleInputChange
+   * @param {*} name 
+   * @param {*} value 
+   */
   const handleInputChange = (name, value) => {
     setFormData((prevData) => ({
       ...prevData,
@@ -21,9 +31,34 @@ function AddListing() {
     console.log(formData);
   };
 
-  const onSubmit = (e) => {
+  /**
+   * Save Features Data using handleFeaturesData
+   * @param {*} name 
+   * @param {*} value 
+   */
+  const handleFeaturesData = (name, value) =>{
+    setFeaturesData((prevData) =>({
+      ...prevData,
+      [name]: value,
+    }));
+    console.log(featuresData)
+  };
+
+  const onSubmit =async(e) => {
     e.preventDefault();
     console.log(formData);
+    try {
+      const result = await db.insert(CarListing).values({
+        ...formData,
+        features:featuresData
+      });
+      if(result){
+        console.log("Data saved succesfully")
+      }
+      
+    } catch (e) {
+      console.log("Error occured while saving data ", e)
+    }
   }
   return (
     <div>
@@ -36,8 +71,9 @@ function AddListing() {
             <h2 className="font-medium text-xl mb-6">Car Details</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {carDetails.carDetails.map((item, index) => (
-                <div key={index} className="">
-                  <label className="text-sm text-gray-600">
+                <div key={index} className="mb-1">
+                  <label className="text-sm text-gray-600 flex items-center gap-2 mb-1">
+                  <IconField icon={item?.icon}/>
                     {item.label}{" "}
                     {item.required && <span className="text-red-600">*</span>}
                   </label>
@@ -63,7 +99,7 @@ function AddListing() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
               {features.features.map((item, index) => (
                 <div key={index} className="flex items-center gap-2">
-                  <Checkbox onCheckedChange={(value)=>(handleInputChange(item.name,value))}/> <h2>{item.label}</h2>
+                  <Checkbox onCheckedChange={(value)=>(handleFeaturesData(item.name,value))}/> <h2>{item.label}</h2>
                 </div>
               ))}
             </div>
