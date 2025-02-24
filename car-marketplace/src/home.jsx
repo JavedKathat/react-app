fe13e0e444e4cd8467aca7c7603d85e0ded451c8
Javed Kathat <javedkhankathat3040@gmail.com>
2025-02-24 08:28:57 +0530

    update home file

--- car-marketplace/src/home.jsx
+++ car-marketplace/src/home.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import MostSearchedCar from "./components/MostSearchedCar"
 // import FakeData from "./Shared/FakeData"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 
 function Home() {
   // console.log(FakeData.carList);
